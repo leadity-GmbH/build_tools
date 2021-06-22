a0a6ef30e5b2165274e47faf7323b8d8d07ab5e4
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2021-06-22 19:20:32 +0200

    owner is used on ssh protocol too

--- scripts/base.py
+++ scripts/base.py
@@ -565,9 +565,9 @@ def get_ssh_base_url():
 def git_update(repo, is_no_errors=False, is_current_dir=False, git_owner=""):
   print("[git] update: " + repo)
   owner = git_owner if git_owner else "ONLYOFFICE"
-  url = git_get_base_url() + owner + "/" + repo + ".git"
-  if git_is_ssh():
-    url = get_ssh_base_url() + repo + ".git"
+  url = "https://github.com/" + owner + "/" + repo + ".git"
+  if config.option("git-protocol") == "ssh":
+    url = "git@github.com:" + owner + "/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
