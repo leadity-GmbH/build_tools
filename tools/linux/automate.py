f6baad3c70fa36a1b5f2fa2e6e51d36dda48d49a
Joud Eskef <joud.eskef@leadity.de>
2026-02-12 16:38:05 +0100

    Fix Qt prebuilt URL (use existing qt_binary_5.9.9_gcc_64.7z)

--- tools/linux/automate.py
+++ tools/linux/automate.py
@@ -57,7 +57,7 @@ def install_qt():
   return
   
 def install_qt_prebuild():
-  url_amd64 = "https://github.com/ONLYOFFICE-data/build_tools_data/raw/refs/heads/master/qt/qt_binary_linux_amd64.7z"
+  url_amd64 = "https://github.com/ONLYOFFICE-data/build_tools_data/raw/refs/heads/master/qt/qt_binary_5.9.9_gcc_64.7z"
   base.download(url_amd64, "./qt_amd64.7z")
   base.extract("./qt_amd64.7z", "./qt_build")
   base.create_dir("./qt_build/Qt-5.9.9")
